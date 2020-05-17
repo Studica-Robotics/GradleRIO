fc19e0ec652c9a4cc85dea28a631b0e1b55ecd63
Scott Libert <scott@kauailabs.com>
2020-05-17 12:42:29 -1000

    Updated to beta 3 (VMX-pi Platform Library v. 1.1.225, VMX-pi WPI Library v. 3.2.2020.vmxpi-beta-1.

--- src/main/groovy/edu/wpi/first/gradlerio/wpi/WPIExtension.groovy
+++ src/main/groovy/edu/wpi/first/gradlerio/wpi/WPIExtension.groovy
@@ -17,8 +17,8 @@ import javax.inject.Inject
 class WPIExtension {
     // WPILib (first.wpi.edu/FRC/roborio/maven) libs
     String wpilibVersion = "2020.3.2"
-    String vmxVersion = "2020.3.2-vmxpi-alpha-6"
-    String vmxPlatformVersion = "1.1.224"
+    String vmxVersion = "2020.3.2-vmxpi-beta-1"
+    String vmxPlatformVersion = "1.1.225"
     String niLibrariesVersion = "2020.10.1"
     String opencvVersion = "3.4.7-2"
     String imguiVersion = "1.72b-1"
